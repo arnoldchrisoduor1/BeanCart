b14a41cde5caf5fa7f45fc1237b645756939aa93
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2025-04-06 23:13:57 +0300

    created product repository

---
+++ backend/src/main/java/com/yourcompany/app/application/dto/ProductCreateDto.java
@@ -0,0 +1,9 @@
+package com.yourcompany.app.application.dto;
+
+import com.yourcompany.app.domain.model.Product;
+import com.yourcompany.app.domain.model.Shop;
+
+import java.util.UUID;
+
+// Dto for creating a pproduct object.
+public class 
