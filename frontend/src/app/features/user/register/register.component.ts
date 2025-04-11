e88cde8a2d65ef1b49a9ca4a4171a1f5037d8cfd
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2025-04-11 09:50:45 +0300

    login and register UI working

--- frontend/src/app/features/user/register/register.component.ts
+++ frontend/src/app/features/user/register/register.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InputComponent } from '../../../shared/components/input/input.component';
+import { ButtonComponent } from '../../../shared/components/button/button.component';
+import { DropdownComponent } from '../../../shared/components/dropdown/dropdown.component';
+import { Mail, Lock, UserCog  } from 'lucide-angular';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule, InputComponent, ButtonComponent, DropdownComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  email: string = '';
+  password: string = '';
+  confirmPassword: string ='';
+  role: string = '';
+  isRegistering: boolean = false;
+  selectedOption: string = '';
 
+  readonly emailIcon = Mail;
+  readonly passwordIcon = Lock;
+  readonly userCog = UserCog;
+
+  onSubmit()  {
+    console.log("Registering attempt with: ", this.email);
+    this.isRegistering = true;
+
+    setTimeout(() => {
+      this.isRegistering = false;
+    }, 2000);
+
+    console.log("done registering");
+  }
 }
