2c18bacd5e61397d1cc94180b0c17e7e9a265220
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2025-04-14 01:41:54 +0300

    version 1 ordered items

--- frontend/src/app/shared/components/stats-box/stats-box.component.ts
+++ frontend/src/app/shared/components/stats-box/stats-box.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-stats-box',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './stats-box.component.html',
   styleUrl: './stats-box.component.css'
 })
 export class StatsBoxComponent {
   @Input() title: string = 'Title';
   @Input() value: number = 0;
+  @Input() status: string = '';
 }
