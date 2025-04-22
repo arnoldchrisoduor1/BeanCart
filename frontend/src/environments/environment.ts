3a1ed41ed94471c260cab47e917ef7cf3ac6c8f5
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2025-04-22 16:36:01 +0300

    implemented nginx in backend to server ssl certificates

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://13.233.223.216:8000' // Your backend API URL
+    apiUrl: 'https://13.233.223.216:8000' // Your backend API URL
   };
