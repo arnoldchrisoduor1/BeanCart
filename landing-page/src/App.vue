615d6ee9f61ec0448bcbf33b267ab4093f8170c0
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2025-04-26 08:44:39 +0300

    last saves before first deployment

--- landing-page/src/App.vue
+++ landing-page/src/App.vue
@@ -1,16 +1,20 @@
 <script setup>
-import Navbar from './components/Navbar.vue'
+import Navbar from "./components/Navbar.vue";
+import Hero from "../src/sections/Hero.vue";
 </script>
 
 <template>
-  <div>
-    <Navbar />
-    <main>
-
-    </main>
+  <div class="">
+    <!-- <div class="bg-indigo-200 absolute inset-0 -z-10"></div> -->
+     <div class="bg-gradient-to-t from-indigo-200 to-white inset-0 absolute -z-20 hidde md:block"></div>
+    <div class="sticky top-2 md:top-4 z-4">
+      <Navbar />
+    </div>
+    <div class="max-w-screen-xl mx-auto">
+      <Hero />
+    </div>
+    <main class="max-w-screen-xl mx-auto"></main>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- landing-page/src/components/Navbar.vue
+++ landing-page/src/components/Navbar.vue
@@ -25,24 +25,40 @@ const isActive = (path) => {
 
 <template>
   <!-- Mobile Menu Toggle Button -->
-  <div class="border border-red-500">
+  <div class="flex justify-between items-center py-2 px-4 w-[95%] md:w-[60%] mx-auto shadow-sm rounded-sm backdrop-blur-[5px] ">
     <div>
-      <p>BeanCart</p>
+      <p class="bg-gradient-to-r from-indigo-500 to-indigo-900 font-bold text-2xl text-transparent bg-clip-text">BeanCart</p>
     </div>
-    <div>
+    <div class="hidden md:block">
+  <ul class="flex flex-row gap-2">
+    <li v-for="item in navItems" :key="item.id">
+      <router-link
+        :to="item.path"
+        class="px-4 py-2 font-semibold transition-colors"
+        :class="isActive(item.path)
+          ? 'bg-indigo-200/50 rounded-full  text-indigo-900'
+          : 'hover:bg-indigo-100/50 rounded-full'"
+      >
+        {{ item.name }}
+      </router-link>
+    </li>
+  </ul>
+</div>
+
+    <div class="flex gap-5 items-center">
       <div>
-        <Bell />
+        <Bell class="text-indigo-900"/>
       </div>
     <button @click="toggleMenu" class="md:hidden  z-40">
-      <AlignJustify v-if="!isMenuOpen" />
+      <AlignJustify v-if="!isMenuOpen" class="text-indigo-900" />
     </button>
   </div>
   </div>
 
   <!-- Black Overlay -->
-  <Transition name="overlay">
-    <div class="absolute bg-black/50 inset-0 z-20" @click="toggleMenu" v-if="isMenuOpen"></div>
-  </Transition>
+  <!-- <Transition name="overlay">
+    <div class="absolute bg-black/50 inset-0 z-30" @click="toggleMenu" v-if="isMenuOpen"></div>
+  </Transition> -->
   <!-- Mobile Navigation Menu -->
   <Transition name="sidebar">
     <div v-if="isMenuOpen" class="md:hidden fixed inset-0 z-30">
@@ -50,8 +66,8 @@ const isActive = (path) => {
       <!-- Sidebar container -->
       <div class="bg-indigo-100 w-[70%] h-screen absolute right-0 top-0 z-30 pt-16 px-5 overflow-hidden">
         <Transition name="title-animation" appear>
-          <div class="absolute top-2 font-bold text-2xl text-indigo-400">
-            <h2>BeanCart</h2>
+          <div class="absolute top-2">
+            <h2 class="bg-gradient-to-r from-indigo-500 to-indigo-900 font-bold text-2xl text-transparent bg-clip-text">BeanCart</h2>
           </div>
         </Transition>
         <div class="absolute top-2 right-2" @click="toggleMenu">
@@ -63,7 +79,7 @@ const isActive = (path) => {
         <TransitionGroup name="list" tag="ul" class="w-full" appear>
           <li v-for="(item, index) in navItems" :key="item.id" :style="{ '--stagger-delay': `${index * 0.1}s` }">
             <router-link :to="item.path"
-              class="flex px-4 py-3 w-full mt-5 rounded-sm shadow-sm font-semibold text-lg text-gray-500" :class="isActive(item.path) ? 'bg-indigo-300' : 'bg-indigo-300/30'
+              class="flex px-4 py-3 w-full mt-5 rounded-sm shadow-sm font-semibold text-lg text-gray-500" :class="isActive(item.path) ? 'bg-gradient-to-r from-indigo-500 to-indigo-900 text-white' : 'bg-gradient-to-r from-indigo-300/20 to-indigo-100/20'
                 " @click="toggleMenu">
               {{ item.name }}
             </router-link>
@@ -71,32 +87,18 @@ const isActive = (path) => {
         </TransitionGroup>
 
         <Transition name="slide-fade" appear>
-          <button class="absolute bottom-24 px-4 py-3 bg-indigo-400 rounded-sm w-[80%] text-center shadow-md">
+          <button class="absolute bottom-24 px-4 py-3 bg-gradient-to-r from-indigo-500 to-indigo-900 rounded-sm w-[80%] text-center shadow-md">
             <p class="font-semibold text-xl text-white">Get Started</p>
           </button>
         </Transition>
 
-        <div class="absolute h-[700px] w-[700px] bg-indigo-200 rounded-full -z-10 -bottom-[200px] -left-[50px]"></div>
+        <div class="absolute h-[700px] w-[700px] bg-radial-[at_25%_25%] from indigo-200 to-indigo-400 to-70% rounded-full -z-10 -bottom-[200px] -left-[50px]"></div>
         <div class="bg-indigo-300 h-[100px] w-[100px] rounded-full absolute bottom-[200px]"></div>
         <div class="bg-indigo-300 h-[50px] w-[50px] rounded-full absolute bottom-[200px] right-[50px]"></div>
         <div class="bg-indigo-300 h-[40px] w-[40px] rounded-full absolute bottom-[300px] right-[25px]"></div>
       </div>
     </div>
   </Transition>
-
-  <!-- Desktop Navigation -->
-  <nav class="hidden md:flex justify-center py-4 bg-indigo-50">
-    <ul class="flex space-x-6">
-      <li v-for="item in navItems" :key="item.id">
-        <router-link :to="item.path" class="px-4 py-2 rounded-md font-medium transition-colors" :class="isActive(item.path)
-          ? 'bg-indigo-300 text-indigo-900'
-          : 'hover:bg-indigo-200'
-          ">
-          {{ item.name }}
-        </router-link>
-      </li>
-    </ul>
-  </nav>
 
   <!-- Content Area -->
   <router-view />
