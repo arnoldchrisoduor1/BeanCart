76bff630b090f3141c4b102801c827ecf649a937
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2025-05-09 19:55:48 +0300

    second deployment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8001',
-    // apiUrl: 'https://api.beancart.shop',
+    // apiUrl: 'http://localhost:8001',
+    apiUrl: 'https://api.beancart.shop',
     stripe: {
       publishableKey: 'pk_test_51RMlYb07UvjDQ744DqZxxLw5fsNseG52cCSTdrzvbdmo8KRVT6898W5aRuRPCE8lfbhgUDgh3L4Bfn9Pilh4zncI00SToaaqBP'  // Replace with your actual key
     }
