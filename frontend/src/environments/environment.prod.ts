2fa6b2e3850d0173a6ecf52c7de8c7f78de45cc7
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2025-05-15 16:06:15 +0300

    home page updated upto get started cta

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://your-production-api-url',// Your production API URL
+    apiUrl: 'https://api.beancart.shop',// Your production API URL
     stripe: {
-      publishableKey: 'pk_test_XXXXXXXXXXXXXXXXXXXXXXXX'  // Replace with your actual key
+      publishableKey: 'pk_test_51RMlYb07UvjDQ744DqZxxLw5fsNseG52cCSTdrzvbdmo8KRVT6898W5aRuRPCE8lfbhgUDgh3L4Bfn9Pilh4zncI00SToaaqBP'  // Replace with your actual key
     }
   };
